f32a01460da4ec4808bc33abd184be229360f158
Yi Yi Htun <yyh@ovaspace.com>
2024-11-03 13:24:48 +0700

    login test case

---
+++ Test Suites/Smoke Test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Smoke Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>26adbd08-8831-446a-9e8a-0d2bee8b14a3</testSuiteGuid>
+</TestSuiteEntity>
